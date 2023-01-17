f3426704cc57b767cdf85185ec4669e706de066b
Suraj <urusanonyms@gmail.com>
2023-01-17 12:18:08 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const startStopBtn = document.getElementById('play');
+const reset = document.getElementById('reset');
+
+let second = 0;
+let minutes = 0;
+let hours = 0;
+
+let leadingSecond = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+function stopWatch() {
+
+    second++;
+
+    if (second / 60 == 1) {
+        second = 0;
+        minutes++;
+    }
+    if (minutes / 60 == 1) {
+        minutes = 0;
+        hours++;
+    }
+    if (second < 10) {
+        leadingSecond = '0' + second;
+    }
+    else {
+        leadingSecond = second;
+    }
+    if (minutes < 10) {
+        leadingMinutes = '0' + minutes;
+    }
+    else {
+        leadingMinutes = minutes;
+    }
+    if (hours < 10) {
+        leadingHours = '0' + hours;
+    }
+    else {
+        leadingHours = hours;
+    }
+
+    let displayTimer = document.getElementById('timer').innerText = leadingHours + ':' + leadingMinutes + ':' + leadingSecond;
+}
+
+startStopBtn.addEventListener('click', function () {
+
+    if (timerStatus === 'stopped') {
+        timerInterval = window.setInterval(stopWatch, 1000);
+        document.getElementById('startstopbtn').innerHTML = `<i class="fa-solid fa-pause" id="pause"></i>`;
+        timerStatus = "started";
+    }
+    else {
+
+        window.clearInterval(timerInterval);
+        document.getElementById('startstopbtn').innerHTML = `<i class="fa-solid fa-play" id="play"></i>`;
+        timerStatus = 'stopped';
+    }
+})
